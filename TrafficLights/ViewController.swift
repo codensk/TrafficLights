33cc98df6ac8bfb7bd19a4ae06e0b46d1e729082
Sergey Vorobev <rusbear28@yandex.ru>
2021-05-18 11:33:56 +0700

    Created traffic lights controller

--- TrafficLights/ViewController.swift
+++ TrafficLights/ViewController.swift
@@ -7,13 +7,69 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+enum TrafficLightColors {
+    case red, orange, green
+}
 
+class ViewController: UIViewController {
+    
+    // MARK: - IB Outlets
+    @IBOutlet weak var redLight: UIView!
+    @IBOutlet weak var orangeLight: UIView!
+    @IBOutlet weak var greenLight: UIView!
+    
+    @IBOutlet weak var trafficLightsControlButton: UIButton!
+    
+    // MARK: - Private Properties
+    private var trafficLightState: TrafficLightColors?
+    
+    // MARK: - Life Cycles Methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
 
+        configureLights()
+        switchOffLights()
+    }
+    
+    // MARK: - IB Actions
+    @IBAction func trafficLightsControlButton(_ sender: UIButton) {
+        switchOffLights()
+        
+        if let currentLightState = trafficLightState {
+            // Traffic lights is on, switch color
+            switch currentLightState {
+            case .red:
+                trafficLightState = .orange
+                orangeLight.alpha = 1
+            case .orange:
+                trafficLightState = .green
+                greenLight.alpha = 1
+            default:
+                trafficLightState = .red
+                redLight.alpha = 1
+            }
+        } else {
+            // Traffic lights is off, change to on
+            trafficLightState = .red
+            redLight.alpha = 1
+            
+            trafficLightsControlButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
+    // MARK: - Private Methods
+    private func configureLights() {
+        redLight.layer.cornerRadius = redLight.frame.height / 2
+        orangeLight.layer.cornerRadius = orangeLight.frame.height / 2
+        greenLight.layer.cornerRadius = greenLight.frame.height / 2
+    }
+    
+    private func switchOffLights() {
+        redLight.alpha = 0.3
+        orangeLight.alpha = 0.3
+        greenLight.alpha = 0.3
+    }
+    
 
 }
 
